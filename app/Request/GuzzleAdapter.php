6e2c800d1eb9a832b51123adcb87217e6054e67a
reactmore <reactmorecom@gmail.com>
2021-07-27 15:24:32 +0700

    Added Request Functions

--- app/Request/GuzzleAdapter.php
+++ app/Request/GuzzleAdapter.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
 use Psr\Http\Message\RequestInterface;
 use GuzzleHttp\Exception\ConnectException;
+use Reactmore\WordpressClient\Helpers\ResponseFormatter;
 
 class GuzzleAdapter implements ClientInterface
 {
@@ -28,13 +29,32 @@ class GuzzleAdapter implements ClientInterface
         $this->guzzle = $client ?: new Client();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function makeUri($uri)
     {
         return new Uri($uri);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function send(RequestInterface $request)
     {
         return $this->guzzle->send($request);
+    }
+
+    public static function handleException($exception)
+    {
+        if ($exception instanceof ConnectException) {
+            return ResponseFormatter::formatResponse([
+                'error' => 'Connection Timeout Error. Please check your internet connection and try again'
+            ], 408, 'failed');
+        } else {
+            return ResponseFormatter::formatResponse([
+                'error' => $exception->getMessage()
+            ], 400, 'failed');
+        }
     }
 }
--- app/Wordpress.php
+++ app/Wordpress.php
@@ -2,8 +2,75 @@
 
 namespace Reactmore\WordpressClient;
 
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
+use Reactmore\WordpressClient\Auth\AuthInterface;
+use Reactmore\WordpressClient\Endpoint;
+use Reactmore\WordpressClient\Request\ClientInterface;
 
-
+/**
+ * Class Wordpress
+ */
 class Wordpress
 {
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @var AuthInterface
+     */
+    private $credentials;
+
+    /**
+     * @var string
+     */
+    private $wordpressUrl;
+
+    /**
+     * @var array
+     */
+    private $endPoints = [];
+
+
+    public function __construct(ClientInterface $httpClient, $wordpressUrl = '')
+    {
+        $this->httpClient = $httpClient;
+        $this->wordpressUrl = $wordpressUrl;
+    }
+
+    /**
+     * @param $wordpressUrl
+     */
+    public function setWordpressUrl($wordpressUrl)
+    {
+        $this->wordpressUrl = $wordpressUrl;
+    }
+
+    /**
+     * @param AuthInterface $auth
+     */
+    public function setCredentials(AuthInterface $auth)
+    {
+        $this->credentials = $auth;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return ResponseInterface
+     */
+    public function send(RequestInterface $request)
+    {
+        if ($this->credentials) {
+            $request = $this->credentials->addCredentials($request);
+        }
+
+        $request = $request->withUri(
+            $this->httpClient->makeUri($this->wordpressUrl . $request->getUri())
+        );
+
+        return $this->httpClient->send($request);
+    }
 }
